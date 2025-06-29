225dc4e42d95ddb11d0d9277c20673ecdf37e030
NullCascade <nullcascade@gmail.com>
2025-06-28 21:16:16 -0700

    Fixed oversight of mining entities with a single minable result not giving the right converted sand/glass item.

--- k2so-tweaks/util/minable.lua
+++ k2so-tweaks/util/minable.lua
@@ -4,6 +4,9 @@ function util_minable.replace_all(old, new)
 	for _, prototype in pairs(data.raw) do
 		for _, entity in pairs(prototype) do
 			if (entity.minable) then
+				if (entity.minable.result == old) then
+					entity.minable.result = new
+				end
 				for _, minable in ipairs(entity.minable.results or {}) do
 					if (minable.name == old) then
 						minable.name = new
