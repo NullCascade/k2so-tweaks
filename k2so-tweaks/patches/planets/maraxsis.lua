e7833176a144789d968159e93338e5005bf79c13
NullCascade <nullcascade@gmail.com>
2025-07-07 14:49:32 -0700

    Re-enabled atmosphere collection in machines other than the atmosphere condensator, until I can playtest Maraxsis and figure out how I want that recipe to integrate with K2SO.

--- k2so-tweaks/patches/planets/maraxsis.lua
+++ k2so-tweaks/patches/planets/maraxsis.lua
@@ -6,7 +6,8 @@ patch:add_required_mod("maraxsis")
 
 function patch.on_data_final_fixes()
 	-- Atmosphere is obtained using atmosphere condensation
-	data.raw["recipe"]["maraxsis-atmosphere"].category = "kr-atmosphere-condensation"
+	-- TODO: The atmosphere condensation machine can't be placed in a way that is compatible with this. Figure it out later.
+	util.recipe.add_additional_category("maraxsis-atmosphere", "kr-atmosphere-condensation")
 
 	-- Electrolysis is done in the electrolysis plant
 	data.raw["recipe"]["salt"].category = "kr-electrolysis"
