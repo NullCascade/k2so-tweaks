f6dc5f33a899aaf6a3b1cbbc31c53dc941c9f8a0
NullCascade <nullcascade@gmail.com>
2025-11-13 00:49:32 -0800

    Muluna: Allow matter conversion to/from alumina.

--- data-updates.lua
+++ data-updates.lua
@@ -2,5 +2,8 @@
 -- Generic patches.
 require("k2so-tweaks.patches.expanded-alternate-recipes")
 
+-- Planet-specific compatibility patches.
+require("k2so-tweaks.patches.planets.muluna")
+
 local util = require("k2so-tweaks.util")
 util.patch.do_data_updates()
--- data.lua
+++ data.lua
@@ -3,6 +3,7 @@ require("k2so-tweaks.patches.mods.crushing-industry-ratios")
 
 -- Planet-specific compatibility patches.
 require("k2so-tweaks.patches.planets.maraxsis")
+require("k2so-tweaks.patches.planets.muluna")
 
 local util = require("k2so-tweaks.util")
 util.patch.do_data()
