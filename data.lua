20fd2baeac6594e2a7da9e0318e39b44792f1abe
NullCascade <nullcascade@gmail.com>
2025-11-13 00:48:52 -0800

    Maraxsis: Allow the advanced chemical plant to be placeable like the normal one.

--- data.lua
+++ data.lua
@@ -1,5 +1,8 @@
 
 require("k2so-tweaks.patches.mods.crushing-industry-ratios")
 
+-- Planet-specific compatibility patches.
+require("k2so-tweaks.patches.planets.maraxsis")
+
 local util = require("k2so-tweaks.util")
 util.patch.do_data()
--- k2so-tweaks/patches/planets/maraxsis.lua
+++ k2so-tweaks/patches/planets/maraxsis.lua
@@ -4,6 +4,25 @@ local util = require("k2so-tweaks.util")
 local patch = util.patch.new_patch("planet-maraxsis")
 patch:add_required_mod("maraxsis")
 
+function copy_buildability_rules(type, from, to)
+	local prototype_from = data.raw[type][from]
+	local prototype_to = data.raw[type][to]
+	if (not prototype_from) then
+		util.log("Could copy maraxsis compatibility from %s: Prototype does not exist.", from, to)
+		return
+	elseif (not prototype_to) then
+		util.log("Could copy maraxsis compatibility to %s: Prototype does not exist.", from, to)
+		return
+	end
+
+	prototype_to.maraxsis_buildability_rules = prototype_from.maraxsis_buildability_rules
+end
+
+function patch.on_data()
+	-- Allow certain other buildings to function in water.
+	copy_buildability_rules("assembling-machine", "chemical-plant", "kr-advanced-chemical-plant")
+end
+
 function patch.on_data_final_fixes()
 	-- Atmosphere is obtained using atmosphere condensation
 	-- TODO: The atmosphere condensation machine can't be placed in a way that is compatible with this. Figure it out later.
