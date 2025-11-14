9394e2539fe04ecad166177a9837119bca7fb551
NullCascade <nullcascade@gmail.com>
2025-11-14 15:13:18 -0800

    Get rid of pumpjack unification patch. K2SO (and K2 itself) both do this by default now.

--- k2so-tweaks/patches/mods/krastorio2-spaced-out.lua
+++ k2so-tweaks/patches/mods/krastorio2-spaced-out.lua
@@ -1,10 +1,3 @@
-
---[[
-	- Switches K2 to use common glass/sand.
-	- Switches K2 to use common oxygen/hydrogen/nitrogen.
-	- Removes the K2 mineral pumpjack and makes the normal pumpjack usable for basic fluids.
---]]
-
 local util = require("k2so-tweaks.util")
 
 local patch = util.patch.new_patch("mod-krastorio2-spaced-out")
@@ -56,26 +49,6 @@ function patch.on_data_final_fixes()
 	replace_k2_fluid("kr-hydrogen", "hydrogen")
 	replace_k2_fluid("kr-nitrogen", "nitrogen")
 	replace_k2_fluid("kr-nitric-acid", "nitric-acid")
-
-	-- Revert back to just one pumpjack. Back when there were only two minable fluids this made more sense.
-	-- As it is, K2SO ends up just using mineral water pumping for everything that isn't oil.
-	-- An alternative would be to make a new pumpjack for every type of fluid, which seems silly.
-	if (data.raw["item"]["kr-mineral-water-pumpjack"]) then
-		-- Get rid of the K2 pumpjack.
-		data.raw["item"]["kr-mineral-water-pumpjack"].hidden = true
-		data.raw["item"]["kr-mineral-water-pumpjack"].hidden_in_factoriopedia = true
-		data.raw["recipe"]["kr-mineral-water-pumpjack"].hidden = true
-		data.raw["recipe"]["kr-mineral-water-pumpjack"].hidden_in_factoriopedia = true
-		util.technology.remove("kr-mineral-water-gathering", "kr-fluids-chemistry")
-
-		-- Restore defaults.
-		data.raw["item"]["pumpjack"].localised_name = { "entity-name.pumpjack" }
-		data.raw["item"]["pumpjack"].icons = nil
-		data.raw["item"]["pumpjack"].icon = "__base__/graphics/icons/pumpjack.png"
-		data.raw["mining-drill"]["pumpjack"].localised_name = { "entity-name.pumpjack" }
-		data.raw["mining-drill"]["pumpjack"].resource_categories = { "basic-fluid" }
-		data.raw["resource"]["crude-oil"].category = "basic-fluid"
-	end
 
 	-- Enforce burn limits to 100 fluid/2 seconds.
 	enforce_burn_limits()
