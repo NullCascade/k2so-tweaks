8a5fe42e6b2f0b899969acf5466e404e2e47f17c
NullCascade <nullcascade@gmail.com>
2025-11-20 02:50:54 -0800

    Age of Production: Added pass to try to ensure that the K2 advanced machines (assembling, furnace, chemical) have access to all the base machine's recipes.

--- k2so-tweaks/patches/mods/age-of-production.lua
+++ k2so-tweaks/patches/mods/age-of-production.lua
@@ -4,6 +4,20 @@ local util = require("k2so-tweaks.util")
 local patch = util.patch.new_patch("mod-age-of-production")
 patch:add_required_mod("Age-of-Production")
 
+local function copy_crafting_categories(from, to)
+	local from_prototype = data.raw["assembling-machine"][from]
+	local to_prototype = data.raw["assembling-machine"][to]
+	if (not from_prototype or not to_prototype) then
+		return
+	end
+
+	for _, category in ipairs(from_prototype.crafting_categories) do
+		if (util.table.insert_unique(to_prototype.crafting_categories, category)) then
+			util.log("Added category '%s' to assembling machine '%s'", category, to)
+		end
+	end
+end
+
 function patch.on_data_final_fixes()
 	-- K2SO Rifle Ammo
 	util.recipe.add_additional_category("kr-armor-piercing-rifle-magazine", "ammunition")
@@ -39,6 +53,11 @@ function patch.on_data_final_fixes()
 	util.recipe.add_additional_category("plutonium-rounds-magazine", "ammunition")
 	util.recipe.add_additional_category("kr-plutonium-rifle-magazine", "ammunition")
 	util.recipe.add_additional_category("kr-plutonium-anti-materiel-rifle-magazine", "ammunition")
+
+	-- Fixup K2's crafting machines to work with AoP's categories.
+	copy_crafting_categories("assembling-machine-3", "kr-advanced-assembling-machine")
+	copy_crafting_categories("chemical-plant", "kr-advanced-chemical-plant")
+	copy_crafting_categories("foundry", "kr-advanced-furnace")
 end
 
 return patch
