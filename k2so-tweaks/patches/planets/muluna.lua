2493d71182e53321dbe70039d8b3f098b53f2217
NullCascade <nullcascade@gmail.com>
2025-12-20 11:54:05 -0800

    Removed the vast majority of Muluna patches in favor of using improved native support between the two mods.

--- k2so-tweaks/patches/planets/muluna.lua
+++ k2so-tweaks/patches/planets/muluna.lua
@@ -1,63 +1,9 @@
---[[
-	- Relaxes surface conditions to allow K2 trains (and other vehicles) to work there.
-
-	TODO:
-		- Add recipe for making processing units using Alumina
---]]
-
 local util = require("k2so-tweaks.util")
 
 local patch = util.patch.new_patch("planet-muluna")
 patch:add_required_mod("planet-muluna")
 
-local gravity_condition = { property = "gravity", min = 0.1 }
-
-local function has_default_gravity_condition(entity)
-	local gravity_condition = util.surface.get_surface_condition(entity, "gravity")
-	if (gravity_condition == nil) then
-		return true
-	end
-
-	return gravity_condition.min == 1.0
-end
-
-local function relax_gravity_conditions(prototype_id, entity_id)
-	if (entity_id) then
-		-- Convert a specific entity
-		local entity = data.raw[prototype_id][entity_id]
-		if (entity and has_default_gravity_condition(entity)) then
-			PlanetsLib.relax_surface_conditions(entity, gravity_condition) --- @diagnostic disable-line
-		end
-	else
-		-- Work on all entities of a given prototype
-		for _, entity in pairs(data.raw[prototype_id] or {}) do
-			if (has_default_gravity_condition(entity)) then
-				PlanetsLib.relax_surface_conditions(entity, gravity_condition) --- @diagnostic disable-line
-			end
-		end
-	end
-end
-
 function patch.on_data_final_fixes()
-	-- Atmosphere is obtained using atmosphere condensation
-	if (data.raw["recipe"]["muluna-carbon-dioxide"]) then
-		data.raw["recipe"]["muluna-carbon-dioxide"].category = "kr-atmosphere-condensation"
-	end
-
-	-- K2's hydrogen and oxygen ratios are odd. We'll buff K2 and hide Maraxsis' recipe.
-	if (data.raw["recipe"]["muluna-electrolysis"]) then
-		data.raw["recipe"]["muluna-electrolysis"].hidden = true
-		data.raw["recipe"]["muluna-electrolysis"].hidden_in_factoriopedia = true
-	end
-	data.raw["recipe"]["kr-water-separation"]["results"] = {
-		{ type = "fluid", name = "oxygen", amount = 100 },
-		{ type = "fluid", name = "hydrogen", amount = 200 },
-	}
-
-	-- Let nuclear trains and other vehicles work on Muluna.
-	relax_gravity_conditions("car", "kr-advanced-tank")
-	relax_gravity_conditions("locomotive", "kr-nuclear-locomotive")
-
 	-- Replace silicon to use K2 silicon.
 	util.item.replace_all("silicon", "kr-silicon")
 end
